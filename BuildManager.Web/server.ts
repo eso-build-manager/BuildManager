790449b7ee5d8b2c3c142fcf43d36159030f2220
bridgerwinter <bridgerwinter@gmail.com>
2024-01-15 22:32:50 -1000

    AB#3 updated tsconfig to add strict warnings, modified server.ts to use correct formatting to adhere to ecma6 standards.

--- BuildManager.Web/server.ts
+++ BuildManager.Web/server.ts
@@ -1,8 +1,6 @@
-import http = require('http');
+import * as http from 'http'; //using improts this way is required to target ECMA scripts, technically slightly slower, but faster.
 const port = process.env.port || 1337
-http.createServer(function (req, res) {
+http.createServer(function (_req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
-
-//test pipeline.web
