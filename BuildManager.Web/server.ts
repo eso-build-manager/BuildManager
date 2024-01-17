00aae0e3a74d33b243ed731d4723fd77c926c60a
bridgerwinter <bridgerwinter@gmail.com>
2024-01-16 18:35:54 -1000

    remove buildmanager.web

--- BuildManager.Web/server.ts
+++
@@ -1,6 +0,0 @@
-import * as http from 'http'; //using improts this way is required to target ECMA scripts, technically slightly slower, but faster.
-const port = process.env.port || 1337
-http.createServer(function (_req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
