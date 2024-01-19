9275f0124c297f7dd7c5db43d038832b6f52a9e7
bridgerwinter <bridgerwinter@gmail.com>
2024-01-18 18:03:30 -1000

    removed content from helloworld.vue to make it clearer what vue-bootstrap was doing.

--- BuildManager.WebApp/buildmanager.webapp.client/src/components/HelloWorld.vue
+++ BuildManager.WebApp/buildmanager.webapp.client/src/components/HelloWorld.vue
@@ -12,30 +12,6 @@
                 <b-dropdown-item disabled>Disabled action</b-dropdown-item>
             </b-dropdown>
         </div>
-        <div v-if="loading" class="loading">
-            Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationvue">https://aka.ms/jspsintegrationvue</a> for more details.
-        </div>
-
-        <div v-if="post" class="content">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="forecast in post" :key="forecast.date">
-                        <td>{{ forecast.date }}</td>
-                        <td>{{ forecast.temperatureC }}</td>
-                        <td>{{ forecast.temperatureF }}</td>
-                        <td>{{ forecast.summary }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
     </div>
 </template>
 
