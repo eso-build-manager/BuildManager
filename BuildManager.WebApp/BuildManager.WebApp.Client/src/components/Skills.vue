8907c21676c63fe8ae68d2a2f68490e8fce14d29
Bridger Winter <bridgerwinter@gmail.com>
2024-02-04 12:38:18 -1000

    added skills dropdown

---
+++ BuildManager.WebApp/BuildManager.WebApp.Client/src/components/Skills.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2> Skills </h2>
+        <div>
+            <b-dropdown  id="skill-dropdown" text="Skills..." class="m-md-2">
+                <b-dropdown-item v-for="skill in skills" :key="skill.skillid">{{skill.name}}</b-dropdown-item>
+            </b-dropdown>
+        </div>
+         <ul>
+             <li>
+                 <h3></h3>
+             </li>
+         </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data(){
+        return {
+            skills: []
+        };
+    },
+    async created() {
+        try {
+            const response = await axios.get('http://localhost:5083/api/skill')
+            this.skills = response.data;
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
+
+</script>
+
+            
